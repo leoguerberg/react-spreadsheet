7f279c8568f5b879f8071000331ae977e82d729f
leoguer <leonardo.guerberg@wearepsh.com>
2021-10-21 23:59:36 -0300

    react + typescript setup

---
+++ src/containers/SpreadsheetContainer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import Spreadsheet from "../views/spreadsheet";
+
+const SpreadsheetContainer = () => {
+    return <Spreadsheet />
+}
+
+export default SpreadsheetContainer;
---
+++ src/views/spreadsheet/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { ISpreadsheetProps } from './types';
+
+const Spreadsheet = (props: ISpreadsheetProps) => {
+return <div>Spreadsheet</div>
+}
+
+export default Spreadsheet;
